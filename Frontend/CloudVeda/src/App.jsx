f4fc999edbf2350a30f05d20620edc61fe6c5403
MR-AMAN123 <mohdaman7054278971@gmail.com>
2025-01-18 02:42:38 +0530

    landing added

--- Frontend/CloudVeda/src/App.jsx
+++ Frontend/CloudVeda/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing1 from './Components/Landing1/Landing.jsx'
+import Landing from './Components/Landing1/Landing.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1>hello</h1>
-    </>
+    <Router>
+    <Routes>
+
+    <Route path="/" element={<Landing/>}></Route>
+    </Routes>
+    
+    
+      
+    </Router>
   )
 }
 
