2f36c65ee20bc9f5eb61bf3f641f01969e552e08
MR-AMAN123 <mohdaman7054278971@gmail.com>
2025-01-18 22:00:00 +0530

    last commit

--- Frontend/CloudVeda/tailwind.config.js
+++ Frontend/CloudVeda/tailwind.config.js
@@ -1,12 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.jsx",
-  ],
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Adjust paths based on your project structure
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        slideOut: {
+          '0%': { transform: 'translateX(0)', opacity: '1' },
+          '100%': { transform: 'translateX(-100%)', opacity: '0' },
+        },
+      },
+      animation: {
+        slideIn: 'slideIn 0.5s ease-out forwards',
+        slideOut: 'slideOut 0.5s ease-out forwards',
+      },
+    },
   },
   plugins: [],
-}
-
+};
