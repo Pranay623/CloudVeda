663ed0d05dd9809461be551f83f9fa8b37f20aaa
Saumyal Baranwal <saumyalbaranwal@gmail.com>
2025-01-18 05:30:04 +0530

    multer  done offline

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import mongoose from 'mongoose';
+import express from 'express'
+import connectDB from './db/connect.js';
+import auth from './router/auth.router.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', auth)
+
+
+// app.use('/api', (req, res, next) => {
+//     console.log("Request Body:", req.body); // Logs incoming data
+//     next();
+// });
+
+
+connectDB();
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(3000, () => {
+      console.log(`Server is running on port ${3000}`)
+    })
+  })
+  // asda
